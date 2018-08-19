009071427ea6c542b2af742e5caa059c819dd94d
seohyundeock <racefl93@naver.com>
2018-08-19 13:56:32 +0900

    set sass & add reset.scss

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
 import registerServiceWorker from './registerServiceWorker';
+import 'styles/reset.scss';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
