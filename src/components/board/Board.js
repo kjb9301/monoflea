070b9128713c443a30948ed19dffb3362f1ab648
cowayzone <cowayzone@naver.com>
2018-09-13 14:01:35 +0900

    design aboutPage

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -7,32 +7,45 @@ const cx = classNames.bind(styles);
 
 const Board = () => {
   return (
-    <div className={cx('board-wrap')}>
-      <Link 
-        className={cx('board-item')} 
-        to="/boards/notice">
-        <span className={cx('board-name')}>공지사항</span>
-      </Link>
-      <Link 
-        className={cx('board-item')}
-        to="/boards/free">
-        <span className={cx('board-name')}>자유게시판</span>
-      </Link>
-      <Link 
-        className={cx('board-item')} 
-        to="/boards/market">
-        <span className={cx('board-name')}>마켓게시판</span>
-      </Link>
-      <Link 
-        className={cx('board-item')} 
-        to="/boards/seller">
-        <span className={cx('board-name')}>셀러게시판</span>
-      </Link>
-      <Link 
-        className={cx('board-item')} 
-        to="/boards/question">
-        <span className={cx('board-name')}>Q&A</span>
-      </Link>
+    <div className={cx('board')}>
+
+      <div className={cx('board-intro')}>
+        <div className={cx('board-intro-head')}><span>Board</span><br/><span className={cx('board-intro-subtitle')}>Monoflea</span></div>
+        <div className={cx('board-intro-body')}>
+          "플리마켓과 원데이 클래스에 대해서 자유롭게 이야기를 나눠요"
+        </div>
+
+        <div className={cx('board-menu')}>
+          <Link 
+            className={cx('board-item')} 
+            to="/boards/notice">
+            <span className={cx('board-name')}>공지사항</span>
+          </Link>
+          <Link 
+            className={cx('board-item')}
+            to="/boards/free">
+            <span className={cx('board-name')}>자유게시판</span>
+          </Link>
+          <Link 
+            className={cx('board-item')} 
+            to="/boards/market">
+            <span className={cx('board-name')}>마켓게시판</span>
+          </Link>
+          <Link 
+            className={cx('board-item')} 
+            to="/boards/seller">
+            <span className={cx('board-name')}>셀러게시판</span>
+          </Link>
+          <Link 
+            className={cx('board-item')} 
+            to="/boards/question">
+            <span className={cx('board-name')}>Q&A</span>
+          </Link>
+        </div>
+
+      </div>
+
+
     </div>
   );
 };
--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -6,12 +6,14 @@ import { Link, NavLink, withRouter } from 'react-router-dom';
 const cx = classNames.bind(styles);
 
 const Header = ({ onLoginClick, onLogout, isLogin, userName, match, onSignupClick }) => {
+  
   const { path } = match;
   return (
     <header className={cx('header')}>
       <div className={cx('header-content')}>
-        <div className={cx('logo', 'space')}>
-          <Link className={cx('logo-text')} to='/'>MonoFlea</Link>
+        <div className={cx('menu-icon')}>☰</div>
+        <div className={cx('logo')}>
+          <Link className={cx('logo-text')} to='/'>MONOFLEA</Link>
         </div>
         {
           path === '/' ? <div className={cx('nav-items')}></div> :
@@ -19,24 +21,21 @@ const Header = ({ onLoginClick, onLogout, isLogin, userName, match, onSignupClic
             <Link className={cx('nav-item')} to='/about'>소개</Link>
             <Link className={cx('nav-item')} to='/boards'>게시판</Link>
             <Link className={cx('nav-item')} to='/sellers'>셀러</Link>
-            <div className={cx('dropdown')}>
-              <button className={cx('dropbtn')}>마켓</button>
-              <div className={cx('dropdown-content')}>
-                <div>
-                  <Link to='/markets'>일정</Link>
-                </div>
-                <div>
-                  <Link to='/markets/recruitment'>등록</Link>
-                </div>
+            {/* <button className={cx('dropbtn')}>마켓</button> */}
+            <div className={cx('nav-item', 'nav-item-market')}>
+              <span>마켓</span>
+              <div className={cx('nav-subitem')}>
+                  <div><Link to='/markets'>일정</Link></div>
+                  <div><Link to='/markets/recruitment'>등록</Link></div>
               </div>
             </div>
             <Link className={cx('nav-item')} to='/classes'>원데이클래스</Link>
           </div>
         }
-        <div className={cx('space')}>
+        {/* <div className={cx('space')}> */}
           {
             isLogin && userName ? 
-            <div>
+            <div  className={cx('header-login')}>
               <div className={cx('header-button')}>
                 {userName}
               </div>
@@ -44,12 +43,12 @@ const Header = ({ onLoginClick, onLogout, isLogin, userName, match, onSignupClic
                 로그아웃
               </div>
             </div> : 
-            <div>
+            <div className={cx('header-login')}>
               <div className={cx('header-button')} onClick={onLoginClick}>로그인</div>
               <div className={cx('header-button')} onClick={onSignupClick}>회원가입</div>            
             </div>
           }
-        </div>
+        {/*</div> */}
       </div>
     </header>
   );
