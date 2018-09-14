d5cd97300c0a8aa65f22d2afe52ad9466f1d8ced
cowayzone <cowayzone@naver.com>
2018-09-14 17:52:51 +0900

    design boardPage

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -15,37 +15,35 @@ const Board = () => {
           "플리마켓과 원데이 클래스에 대해서 자유롭게 이야기를 나눠요"
         </div>
 
-        <div className={cx('board-menu')}>
-          <Link 
-            className={cx('board-item')} 
-            to="/boards/notice">
-            <span className={cx('board-name')}>공지사항</span>
-          </Link>
-          <Link 
-            className={cx('board-item')}
-            to="/boards/free">
-            <span className={cx('board-name')}>자유게시판</span>
-          </Link>
-          <Link 
-            className={cx('board-item')} 
-            to="/boards/market">
-            <span className={cx('board-name')}>마켓게시판</span>
-          </Link>
-          <Link 
-            className={cx('board-item')} 
-            to="/boards/seller">
-            <span className={cx('board-name')}>셀러게시판</span>
-          </Link>
-          <Link 
-            className={cx('board-item')} 
-            to="/boards/question">
-            <span className={cx('board-name')}>Q&A</span>
-          </Link>
+        <div className={cx('board-menu-wrap')}>
+          <div className={cx('board-menu')}>
+              <Link
+                className={cx('board-item','board-free')} 
+                to="/boards/free"><span><strong>자유게시판</strong></span>
+              </Link>
+              <Link
+                className={cx('board-item','board-market')}
+                to="/boards/market">
+                <span className={cx('board-name')}><strong>마켓게시판</strong></span>
+              </Link>
+              <Link 
+                className={cx('board-item','board-seller')}
+                to="/boards/seller">
+                <span className={cx('board-name')}><strong>셀러게시판</strong></span>
+              </Link>
+              <Link
+                className={cx('board-item','board-notice')}
+                to="/boards/notice">
+                <span className={cx('board-name')}><strong>공지사항</strong></span>
+              </Link>
+              <Link
+                className={cx('board-item','board-qna')}
+                to="/boards/question">
+                <span className={cx('board-name')}><strong>Q&A</strong></span>
+              </Link>
+          </div>
         </div>
-
       </div>
-
-
     </div>
   );
 };
